ca2c04dddde5a9ea27f8172dcb75ca67a5704cbe
bmuday <bmuday@live.fr>
2022-08-31 21:30:02 +0000

    reinit folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Form from "./components/Form";
+import PicCard from "./components/PicCard";
+
 const App = () => {
+  const [picsData, setPicsData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/pictures")
+      .then((res) => setPicsData(res.data));
+  }, []);
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1>NFT Gallery</h1>
+      <Form />
+      <div className="cards-container">
+        {picsData?.map((pic, index) => (
+          <PicCard key={index} pic={pic} />
+        ))}
+      </div>
     </>
   );
 };
